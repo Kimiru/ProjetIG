acf2560269cdba3c89e155c7bcef128c0f71d1c1
rmeniche <robin.menichetti@edu.univ-fcomte.fr>
2021-04-11 11:43:34 +0200

    add positionupdate class

---
+++ src/Scenery/PositionUpdater.cpp
@@ -0,0 +1,55 @@
+#include <Scenery/PositionUpdater.hpp>
+
+/**
+* Compute the relative movement function of
+* the velocity, the acceleration, the drag and the deltaTime
+* deleta = vel*dt + acc*dt + min(vel*mindrag, mindrag)*dt
+*/
+void computeDelta(float dt) {
+
+}
+
+/**
+ * Apply the delta to the position
+ * Saving the last position
+ */
+void applyDelta() {
+
+}
+
+/**
+ * Reverse position to the last position
+ */
+void reverseDelta() {
+
+}
+
+/**
+ * Reapply the x member of the delta
+ */
+void redoX() {
+
+}
+
+/**
+ * Reapply the y member of the delta
+ */
+
+void redoY() {
+
+}
+
+/**
+ * Reapply the z member of the delta
+ */
+void redoZ() {
+
+}
+
+/**
+ * Update the position
+ * Calculate the delta and apply it
+ */
+void update(float dt) {
+
+}
